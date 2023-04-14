3395208a02b328dc7cc93659288593b8e48f5d5f
Hasan Kılıç <97hasankilic@gmail.com>
2023-04-15 02:04:53 +0300

    Basic Main Page designed

--- App.js
+++ App.js
@@ -1,11 +1,34 @@
-import React from "react";
-import { View, Text, SafeAreaView } from "react-native";
+import React from 'react'
+import { View, Text, SafeAreaView, StyleSheet } from 'react-native'
 
 const App = () => {
-  return(
-    <SafeAreaView>
-      <Text>Current WeatherApp</Text>
+  return (
+    <SafeAreaView style={styles.wrapper}>
+      <View style={styles.container}>
+        <Text style={styles.temp}>6</Text>
+        <Text style={styles.feels}>Feels like 5</Text>
+        <Text>High: 9, Low: 6</Text>
+      </View>
     </SafeAreaView>
   )
 }
-export default App; 
+
+const styles = StyleSheet.create({
+  wrapper:{
+    flex: 1,
+  },
+  container:{
+    backgroundColor:'lightblue',
+    flex: 1,
+    alignItems: 'center',
+  },
+  temp:{
+    fontSize: 48,
+    color: 'black',
+  },
+  feels:{
+    fontSize: 30,
+    color: 'black',
+  }
+})
+export default App
