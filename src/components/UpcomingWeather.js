f2fb60c3973adf4e55bd808aed0e1f1261120d73
Hasan Kılıç <97hasankilic@gmail.com>
2023-04-17 02:09:09 +0300

    New component is re-designed with sample data

--- src/components/UpcomingWeather.js
+++ src/components/UpcomingWeather.js
@@ -1,20 +1,124 @@
-import React from "react"
-import { SafeAreaView, StyleSheet, Text } from "react-native"
+import React from 'react'
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  FlatList,
+  View,
+  StatusBar,
+  ImageBackground
+} from 'react-native'
+import { Feather } from '@expo/vector-icons'
+
+const DATA = [
+  {
+    dt_txt: '2023-02-18 12:00:00',
+    main: {
+      temp_max: 8.55,
+      temp_min: 7.55
+    },
+    weather: [
+      {
+        main: 'Clear'
+      }
+    ]
+  },
+  {
+    dt_txt: '2023-02-18 15:00:00',
+    main: {
+      temp_max: 8.55,
+      temp_min: 7.55
+    },
+    weather: [
+      {
+        main: 'Cloud'
+      }
+    ]
+  },
+  {
+    dt_txt: '2023-02-18 18:00:00',
+    main: {
+      temp_max: 8.55,
+      temp_min: 7.55
+    },
+    weather: [
+      {
+        main: 'Rain'
+      }
+    ]
+  }
+]
+
+const Item = (props) => {
+  const { dt_txt, min, max, condition } = props
+  return (
+    <View style={styles.item}>
+      <Feather name={'sun'} size={50} color={'white'} />
+      <Text style={styles.date}>{dt_txt}</Text>
+      <Text style={styles.temp}>{min}</Text>
+      <Text style={styles.temp}>{max}</Text>
+    </View>
+  )
+}
+
 const UpcomingWeather = () => {
-    return (
-        <SafeAreaView style = {styles.container}>
-            <Text>
-                Upcoming Weather
-            </Text>
-        </SafeAreaView>
-    )
+  const renderItem = ({ item }) => (
+    <Item
+      condition={item.weather[0].main}
+      dt_txt={item.dt_txt}
+      min={item.main.temp_min}
+      max={item.main.temp_max}
+    />
+  )
+  return (
+    <SafeAreaView style={styles.container}>
+      <ImageBackground
+        source={require('../../assets/upcoming-back.jpeg')}
+        style={styles.image}
+      >
+        <Text style={styles.title}>Upcoming Weather</Text>
+        <FlatList
+          renderItem={renderItem}
+          data={DATA}
+          keyExtractor={(item) => item.dt_txt}
+        />
+      </ImageBackground>
+    </SafeAreaView>
+  )
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-
-    }
+  title: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    color: 'white'
+  },
+  container: {
+    flex: 1,
+    marginTop: StatusBar.currentHeight || 0,
+    backgroundColor: 'royalblue'
+  },
+  item: {
+    padding: 20,
+    marginVertical: 4,
+    marginHorizontal: 8,
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    borderWidth: 5,
+    backgroundColor: 'pink'
+  },
+  temp: {
+    fontSize: 20,
+    color: 'white'
+  },
+  date: {
+    fontSize: 15,
+    color: 'white'
+  },
+  image: {
+    flex: 1
+  }
 })
 
 export default UpcomingWeather
